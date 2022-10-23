b4eb95d8f7926fa36f83b512b3def521ddcd191c
rlapo213 <rlapo@naver.com>
2022-10-23 11:40:09 +0900

    databinding

---
+++ src/views/DataBindingStringView.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+    data() {
+        return{
+            username:"jaehyun"
+        }
+    }
+}
+</script>
+
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
